337238527e7d3b8138fddf765c81cef3c7c0ea99
Stygian <stygian.warlock.v2@gmail.com>
2014-03-06 15:13:59 -0600

    Initial commit for forum package

---
+++ src/Syntax/Forum/ForumServiceProvider.php
@@ -0,0 +1,68 @@
+<?php namespace Syntax\Forum;
+
+use Illuminate\Support\ServiceProvider;
+
+class ForumServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('syntax/forum');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->shareWithApp();
+		$this->registerViews();
+	}
+
+	/**
+	 * Share the package with application
+	 *
+	 * @return void
+	 */
+	protected function shareWithApp()
+	{
+		$this->app['forum'] = $this->app->share(function($app)
+		{
+			return true;
+		});
+	}
+
+	/**
+	 * Register views
+	 *
+	 * @return void
+	 */
+	protected function registerViews()
+	{
+		$this->app['view']->addNamespace('forum', __DIR__.'/../../../views');
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
